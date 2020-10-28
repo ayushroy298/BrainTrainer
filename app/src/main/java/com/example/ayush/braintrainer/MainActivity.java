dfa6adc13bb1a175af8fabaa0e6e769773cea8ae
Ayushroysrm <73576915+Ayushroysrm@users.noreply.github.com>
2020-10-28 12:16:11 +0530

    E4

--- app/src/main/java/com/example/ayush/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/ayush/braintrainer/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     Question question[]=new Question[5];
 
     int quesno;
+    int ansno;
     TextView questionTextView;
     TextView op1,op2,op3,op4;
     int totalques;
